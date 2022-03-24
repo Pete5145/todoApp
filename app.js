2e2911e22a748cf40d17a90c2ee3cb1e4148de81
Pete5145 <peterosakpolor661@gmail.com>
2022-03-23 17:31:25 -0700

    second-commit

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
 let input = document.querySelector(".txt-input");
 let btn = document.querySelector(".btn");
-let myUl = document.querySelector(".myUl")
+let todoList = document.querySelector(".todoList");
+let form = document.querySelector(".form");
 
 btn.addEventListener("click", function() {
-    let li = document.createElement("li");
+    let todoItem = document.createElement("li");
     let inputValue = input.value;
-    let t = document.createTextNode(inputValue);
-    li.appendChild(t);
+    let todoText = document.createTextNode(inputValue);
+    todoItem.appendChild(todoText);
 
     if (inputValue === "") {
         alert("You must write something!");
     } else {
-        myUl.appendChild(li);
+        todoList.appendChild(todoItem);
         input.value = "";
     }
 
     let span = document.createElement("span");
     span.className = "close fa-solid fa-xmark";
-    li.appendChild(span);
+    todoItem.appendChild(span);
 
     span.addEventListener("click", function() {
-        let div = this.parentElement;
-        div.style.display = "none";
+        let closeIcon = this.parentElement;
+        closeIcon.style.display = "none";
     });
 
     let italic = document.createElement("I");
     italic.className = "check fa-solid fa-check";
-    li.appendChild(italic);
+    todoItem.appendChild(italic);
 
     italic.addEventListener("click", function() {
-        let div = this.parentElement;
-        div.style.textDecoration = "line-through";
+        let checkIcon = this.parentElement;
+        checkIcon.style.textDecoration = "line-through";
     });
+});
+
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
 });
 
